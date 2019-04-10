9fe7328f3af74791c17b6ff5e447371006020c4a
Asif Mahmud <asif01050105@gmail.com>
2019-04-10 17:31:04 +0600

    first commit

---
+++ Python Tutorial/hello.py
@@ -0,0 +1,12 @@
+# msg= "Hello "
+# print(msg)
+# print(2+5)
+# a=5
+# b=8
+# c=a+b
+# print("Sum is ", c)
+
+a =6
+b=10
+if(a>b): {print(a ," is greater than " ,b)}
+else : {print(b ," is greater than " ,a)}
