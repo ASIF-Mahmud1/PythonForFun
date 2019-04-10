93e4b429b0b1a5e4c360d26ed7fd3a19a00e8487
Asif Mahmud <asif01050105@gmail.com>
2019-04-10 18:05:17 +0600

    learning numpy

---
+++ Python Tutorial/NumPy/learnNumpy.py
@@ -0,0 +1,17 @@
+# from numpy import array
+# l= [1,2,3,4,5]
+# a= array(l)
+# print(a)
+
+# print(a.shape)
+
+import numpy as np 
+a = np.array([1,2,3]) 
+# print (a)
+
+A= np.array([ [1,2],[3,4],[5,6] ])
+B= np.array([ [1,2],[3,4] ])
+print(B)
+C= A.dot(B)
+D= A@B
+print("D is \n",D)
